0ef007caf739219fa03fba80b49b3f7487480ec9
LuizGVValentin1999 <luiz.gustavo1999@hotmail.com>
2022-07-02 12:26:32 -0300

    Removendo o que não esta sendo utilizado

--- src/views/pages/authentication/authentication3/Login3.js
+++ src/views/pages/authentication/authentication3/Login3.js
@@ -9,7 +9,6 @@ import AuthWrapper1 from '../AuthWrapper1';
 import AuthCardWrapper from '../AuthCardWrapper';
 import AuthLogin from '../auth-forms/AuthLogin';
 import Logo from 'ui-component/Logo';
-import AuthFooter from 'ui-component/cards/AuthFooter';
 
 // assets
 
@@ -55,21 +54,6 @@ const Login = () => {
                                     <Grid item xs={12}>
                                         <AuthLogin />
                                     </Grid>
-                                    <Grid item xs={12}>
-                                        <Divider />
-                                    </Grid>
-                                    {/* <Grid item xs={12}>
-                                        <Grid item container direction="column" alignItems="center" xs={12}>
-                                            <Typography
-                                                component={Link}
-                                                to="/pages/register/register3"
-                                                variant="subtitle1"
-                                                sx={{ textDecoration: 'none' }}
-                                            >
-                                                Don&apos;t have an account?
-                                            </Typography>
-                                        </Grid>
-                                    </Grid> */}
                                 </Grid>
                             </AuthCardWrapper>
                         </Grid>
